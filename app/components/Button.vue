e65b38051694383ed9e92cd174496b00b4720262
Jonelskie <jonel.uligan7@gmail.com>
2026-01-29 20:24:08 +0800

    refactor: update Button and CallToAction components for improved styling and structure

--- app/components/Button.vue
+++ app/components/Button.vue
@@ -1,7 +1,16 @@
 <template>
-	<button
-		class="flex gap-3 items-center min-h-7 p-2 px-7 border-none dark:border select-none rounded-lg bg-primary text-white hover:bg-secondary-dark/50 dark:hover:bg-primary/70 transition shadow-lg dark:shadow-none hover:cursor-pointer">
-		<slot name="icon" />
-		<slot />
-	</button>
+  <button
+    :class="`flex gap-3 w-full md:w-auto items-center min-h-auto md:min-h-7 justify-center p-2 px-7 font-semibold border-none dark:border dark:text-slate-900 select-none rounded-lg bg-primary text-white hover:bg-secondary-dark/50 dark:hover:bg-primary/70 transition shadow-lg dark:shadow-none hover:cursor-pointer ${props.class}`">
+    <slot name="icon" />
+    <slot />
+  </button>
 </template>
+
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+    default: ''
+  }
+});
+</script>
