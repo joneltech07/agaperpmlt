72774dcb7355669665556bf1d89956a948274aa3
Jonelskie <jonel.uligan7@gmail.com>
2026-01-30 22:50:56 +0800

    fix Banner height for mobile view.

--- app/components/Banner.vue
+++ app/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex h-80 bg-secondary-light dark:bg-slate-800 text-center justify-center items-center"
+    <div class="flex px-32 bg-secondary-light dark:bg-slate-800 text-center justify-center items-center"
         :style="{ backgroundImage: `url(${props.backgroundImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }">
         <h1 class="text-center text-white font-bold text-5xl font-poppins">{{ props.title.toUpperCase() }}</h1>
     </div>
@@ -22,5 +22,12 @@ const props = defineProps({
 div {
     padding-top: 128px;
     padding-bottom: 128px;
+}
+
+@media (max-width: 768px) {
+    div {
+        padding-top: 64px;
+        padding-bottom: 64px;
+    }
 }
 </style>
