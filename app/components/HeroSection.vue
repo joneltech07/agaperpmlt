5cca29e42746fdee9a79d43acf2f546a5117a5ba
Jonelskie <jonel.uligan7@gmail.com>
2026-01-30 22:46:47 +0800

    Improve Hero text alignement for mobile view.

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -2,7 +2,7 @@
     <div class="h-70 md:h-167.5 p-0 w-full relative bg-primary dark:bg-slate-800">
         <img src="/images/hero1.jpg" alt="Hero Image" class="w-full h-70 md:h-full object-cover opacity-50">
         <div
-            class="flex flex-col absolute w-full top-40 left-1/2 md:top-1/2 md:left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 text-white text-center text-shadow-lg">
+            class="flex flex-col absolute w-full top-30 left-1/2 md:top-1/2 md:left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 text-white text-center text-shadow-lg">
             <h1 class="font-poppins text-5xl font-bold">
                 AGAPE REACHING PEOPLE MINISTRY
             </h1>
