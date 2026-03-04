93eebdd83450c8c6b618946892295203cebdc079
joneltech <jonel.uligan7@gmail.com>
2026-03-05 05:40:19 +0800

    remove primary bg filter in hero section.

--- app/components/HeroSection.vue
+++ app/components/HeroSection.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="h-70 md:h-167.5 p-0 w-full relative bg-primary dark:bg-slate-800 overlow-hidden">
-		<img src="/images/agape.jpg" alt="Hero Image" class="w-full h-70 md:h-full object-cover opacity-50 select-none pointer-events-none"
-			draggable="false" ondragstart="return false;" />
+	<div class="h-70 md:h-167.5 p-0 w-full relative bg-slate-800 overlow-hidden">
+		<img src="/images/agape.jpg" alt="Hero Image"
+			class="w-full h-70 md:h-full object-cover opacity-50 select-none pointer-events-none" draggable="false"
+			ondragstart="return false;" />
 		<div
 			class="flex flex-col absolute w-full top-30 left-1/2 md:top-1/2 md:left-1/2 transform -translate-x-1/2 md:-translate-y-1/2 text-white text-center text-shadow-lg">
 			<h1 class="font-poppins text-2xl md:text-5xl font-bold">
