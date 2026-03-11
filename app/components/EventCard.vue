9047894a4ab8b59c1f91dc6770383c84479dba2c
joneltech <jonel.uligan7@gmail.com>
2026-03-12 05:05:49 +0800

    Add EventCard component and integrate it into events page for dynamic event display.

---
+++ app/components/EventCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    :class="`flex flex-col gap-2 p-2 bg-white rounded shadow-lg dark:bg-gray-800 ${customClass}`"
+  >
+    <NuxtImg
+      :src="event.image.src"
+      :alt="event.image.alt"
+      width="200"
+      height="300"
+      :custom="true"
+      v-slot="{ src, isLoaded, imgAttrs }"
+    >
+      <!-- Show the actual image when loaded -->
+      <img
+        v-if="isLoaded"
+        v-bind="imgAttrs"
+        :src="src"
+        class="rounded w-full h-60 object-cover object-center"
+      />
+
+      <!-- Show a placeholder while loading -->
+      <USkeleton v-else class="w-full h-60 rounded" />
+    </NuxtImg>
+
+    <div class="flex flex-col gap-2">
+      <h2 class="text-primary font-bold">{{ event.title }}</h2>
+      <div class="text-xs flex gap-1 items-center">
+        <UIcon name="i-lucide-map-pin" class="inline-block mr-1" />
+        {{ event.location }}
+      </div>
+
+      <div class="text-xs flex gap-1 items-center">
+        <UIcon name="i-lucide-calendar" class="inline-block mr-1" />
+        {{ event.date }}
+      </div>
+
+      <NuxtLink
+        :to="event.link"
+        class="rounded bg-white shadow px-3 py-2 text-slate-700 flex items-center justify-center gap-1 font-bold text-xs self-stretch md:self-start"
+      >
+        <UIcon name="i-lucide-external-link" class="inline-block" /> Learn More
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  event: {
+    title: string
+    location: string
+    date: string
+    image: {
+      src: string
+      alt: string
+    }
+    link: string
+  }
+  customClass: string
+}>()
+</script>
--- app/pages/events/index.vue
+++ app/pages/events/index.vue
@@ -1,3 +1,54 @@
 <template>
+  <section>
     <Banner title="AGAPERPMLT Events" />
+
+    <Section>
+      <div class="flex flex-wrap justify-center gap-5">
+        <EventCard
+          v-for="event in events"
+          :key="event.title"
+          customClass="w-70"
+          :event="event"
+        />
+      </div>
+    </Section>
+  </section>
 </template>
+
+<script setup lang="ts">
+import { link } from '#build/ui'
+import Section from '~/layouts/Section.vue'
+
+const events = [
+  {
+    title: 'Community Outreach',
+    location: 'La Trinidad, Benguet',
+    date: 'Saturday, July 15, 2024',
+    image: {
+      src: 'logo.png',
+      alt: 'Community Outreach'
+    },
+    link: '/events/community-outreach'
+  },
+  {
+    title: 'Youth Retreat',
+    location: 'Baguio City',
+    date: 'Saturday, August 20, 2024',
+    image: {
+      src: '/images/youth_retreat.jpg',
+      alt: 'Youth Retreat'
+    },
+    link: '/events/community-outreach'
+  },
+  {
+    title: 'Worship Night',
+    location: 'La Trinidad, Benguet',
+    date: 'Saturday, September 10, 2024',
+    image: {
+      src: '/images/worship_night.jpg',
+      alt: 'Worship Night'
+    },
+    link: '/events/community-outreach'
+  }
+]
+</script>
