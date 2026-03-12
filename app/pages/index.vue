1ff69d293f39b1d4461b119060c483bb61497264
joneltech <jonel.uligan7@gmail.com>
2026-03-13 03:44:04 +0800

    Update spacing in index page layout for improved visual hierarchy.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<main class="min-h-screen flex flex-col justify-center gap-5 md:gap-10 pb-20">
 		<HeroSection />
-		<div class="p-3 md:p-5 md:px-5 lg:px-15 flex flex-col gap-5 md:gap-10">
+		<div class="p-3 md:p-5 md:px-5 lg:px-15 flex flex-col gap-10 md:gap-20">
 			<CallToActionSection />
 			<ServiceInfoSection />
 			<ChurchIntroSection />
