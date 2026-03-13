8b47f3c8c190ed53b834c76525e46c2e9a09ca2e
joneltech <jonel.uligan7@gmail.com>
2026-03-13 23:25:46 +0800

    Add dark mode support to vue-cal component and update styles.

--- app/pages/events/index.vue
+++ app/pages/events/index.vue
@@ -7,6 +7,7 @@
 
       <div class="w-full mt-20">
         <vue-cal
+          :dark="isDark"
           view="month"
           :views="['week', 'month']"
           :events="
@@ -27,4 +28,32 @@
 import Section from '~/layouts/Section.vue'
 import { VueCal } from 'vue-cal'
 import 'vue-cal/style'
+
+const isDark = ref(document.documentElement.classList.contains('dark'))
+
+const observer = new MutationObserver(() => {
+  const getIsDark = document.documentElement.classList.contains('dark')
+  isDark.value = getIsDark
+})
+
+observer.observe(document.documentElement, {
+  attributes: true,
+  attributeFilter: ['class']
+})
 </script>
+
+<style scoped>
+/* Light Mode */
+.vuecal {
+  --vuecal-primary-color: #d3a438;
+  --vuecal-bg-color: #ffffff;
+  --vuecal-text-color: #1f2937;
+}
+
+/* Dark Mode */
+.dark .vuecal {
+  --vuecal-primary-color: #d3a438;
+  --vuecal-bg-color: #111827;
+  --vuecal-text-color: #f9fafb;
+}
+</style>
